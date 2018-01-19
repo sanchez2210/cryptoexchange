1f6f7c375a331191631ea71ff514278a1e2a2bdb
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2018-01-18 23:03:49 -0500

    Enable querying for bittrex complete ticker summary
    It's nice to be able to query for a ticker summary to limit the amount
    of api calls that the client makes. If you are trying to compare prices
    between different exchanges then it becomes really necessary to be able
    to do this efficiently.

--- lib/cryptoexchange/exchanges/bittrex/services/market.rb
+++ lib/cryptoexchange/exchanges/bittrex/services/market.rb
@@ -8,21 +8,45 @@ module Cryptoexchange::Exchanges
           end
         end
 
-        def fetch(market_pair)
-          output = super(ticker_url(market_pair))
-          adapt(output, market_pair)
+        def fetch(market_pair = nil)
+          if market_pair
+            output = super(ticker_url(market_pair))['result'][0]
+            adapt(output, market_pair)
+          else
+            output = super(self.ticker_url)['result']
+            adapt_all(output)
+          end
         end
 
-        def ticker_url(market_pair)
-          base = market_pair.base
-          target = market_pair.target
-          # Bittrex pair has BTC comes first, when BTC is typically a Target not a Base
-          "#{Cryptoexchange::Exchanges::Bittrex::Market::API_URL}/public/getmarketsummary?market=#{target}-#{base}"
+        def adapt_all(output)
+          output.map do |market|
+            # Target comes first in Bittrex ie. BTC-BCN
+            # BTC cannot be a base in this pair
+            target, base = market['MarketName'].split('-')
+            market_pair = Cryptoexchange::Models::MarketPair.new(
+                            base: base,
+                            target: target,
+                            market: Bittrex::Market::NAME
+                          )
+
+            adapt(market, market_pair)
+          end
+        end
+
+        def ticker_url(market_pair = nil)
+          if market_pair
+            base = market_pair.base
+            target = market_pair.target
+            # Bittrex pair has BTC comes first, when BTC is typically a Target not a Base
+            "#{Cryptoexchange::Exchanges::Bittrex::Market::API_URL}/public/getmarketsummary?market=#{target}-#{base}"
+          else
+            "#{Cryptoexchange::Exchanges::Bittrex::Market::API_URL}/public/getmarketsummaries"
+          end
         end
 
         def adapt(output, market_pair)
           ticker = Cryptoexchange::Models::Ticker.new
-          market = output['result'][0]
+          market = output
 
           ticker.base      = market_pair.base
           ticker.target    = market_pair.target
