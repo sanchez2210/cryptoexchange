3771d96296623a9c7c5769121580e163314bd3b6
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2018-01-19 00:27:00 -0500

    Permit querying for cryptopia markets summary

--- lib/cryptoexchange/exchanges/cryptopia/services/market.rb
+++ lib/cryptoexchange/exchanges/cryptopia/services/market.rb
@@ -8,34 +8,54 @@ module Cryptoexchange::Exchanges
           end
         end
 
-        def fetch(market_pair)
-          output = super(ticker_url(market_pair))
-          adapt(output)
+        def fetch(market_pair = nil)
+          if market_pair
+            output = super(ticker_url(market_pair))['Data']
+            adapt(output, market_pair)
+          else
+            output = super(self.ticker_url)['Data']
+            adapt_all(output)
+          end
         end
 
-        def ticker_url(market_pair)
-          base = market_pair.base
-          target = market_pair.target
-          "#{Cryptoexchange::Exchanges::Cryptopia::Market::API_URL}/GetMarket/#{base}_#{target}"
+        def adapt_all(output)
+          output.map do |market|
+            base, target = market['Label'].split('/')
+            market_pair = Cryptoexchange::Models::MarketPair.new(
+                            base: base,
+                            target: target,
+                            market: Cryptopia::Market::NAME
+                          )
+
+            adapt(market, market_pair)
+          end
         end
 
-        def adapt(output)
-          data = output['Data']
-          base, target = data['Label'].split('/')
+        def ticker_url(market_pair = nil)
+          if market_pair
+            base = market_pair.base
+            target = market_pair.target
+            "#{Cryptoexchange::Exchanges::Cryptopia::Market::API_URL}/GetMarket/#{base}_#{target}"
+          else
+            "#{Cryptoexchange::Exchanges::Cryptopia::Market::API_URL}/GetMarkets/"
+          end
+        end
 
+        def adapt(output, market_pair)
           ticker           = Cryptoexchange::Models::Ticker.new
-          ticker.base      = base
-          ticker.target    = target
+
+          ticker.base      = market_pair.base
+          ticker.target    = market_pair.target
           ticker.market    = Cryptopia::Market::NAME
-          ticker.last      = NumericHelper.to_d(data['LastPrice'])
-          ticker.bid       = NumericHelper.to_d(data['BidPrice'])
-          ticker.ask       = NumericHelper.to_d(data['AskPrice'])
-          ticker.high      = NumericHelper.to_d(data['High'])
-          ticker.low       = NumericHelper.to_d(data['Low'])
-          ticker.volume    = NumericHelper.to_d(data['Volume'])
-          ticker.change    = NumericHelper.to_d(data['Change'])
+          ticker.last      = NumericHelper.to_d(output['LastPrice'])
+          ticker.bid       = NumericHelper.to_d(output['BidPrice'])
+          ticker.ask       = NumericHelper.to_d(output['AskPrice'])
+          ticker.high      = NumericHelper.to_d(output['High'])
+          ticker.low       = NumericHelper.to_d(output['Low'])
+          ticker.volume    = NumericHelper.to_d(output['Volume'])
+          ticker.change    = NumericHelper.to_d(output['Change'])
           ticker.timestamp = Time.now.to_i
-          ticker.payload   = data
+          ticker.payload   = output
           ticker
         end
       end
