f3f59295e155e0c585d311e37e3b727497721169
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2018-01-19 02:21:12 -0500

    Allow querying for Binance full markets summary

--- lib/cryptoexchange/exchanges/binance/services/market.rb
+++ lib/cryptoexchange/exchanges/binance/services/market.rb
@@ -8,15 +8,40 @@ module Cryptoexchange::Exchanges
           end
         end
 
-        def fetch(market_pair)
-          output = super(ticker_url(market_pair))
-          adapt(output, market_pair)
+        def fetch(market_pair = nil)
+          if market_pair
+            output = super(ticker_url(market_pair))
+            adapt(output, market_pair)
+          else
+            output = super(self.ticker_url)
+            adapt_all(output)
+          end
         end
 
-        def ticker_url(market_pair)
-          base = market_pair.base
-          target = market_pair.target
-          "#{Cryptoexchange::Exchanges::Binance::Market::API_URL}/ticker/24hr?symbol=#{base}#{target}"
+        def ticker_url(market_pair = nil)
+          if market_pair
+            base = market_pair.base
+            target = market_pair.target
+            "#{Cryptoexchange::Exchanges::Binance::Market::API_URL}/ticker/24hr?symbol=#{base}#{target}"
+          else
+            "#{Cryptoexchange::Exchanges::Binance::Market::API_URL}/ticker/24hr"
+          end
+        end
+
+        def adapt_all(output)
+          targets = ['BTC', 'BNB', 'ETH']
+
+          output.map do |market|
+            target = targets.find { |t| /#{t}$/ === market['symbol'] }
+            base = market['symbol'].sub(/#{target}$/, '')
+
+            market_pair = Cryptoexchange::Models::MarketPair.new(
+              base: base,
+              target: target,
+              market: Binance::Market::NAME
+            )
+            adapt(market, market_pair)
+          end
         end
 
         def adapt(output, market_pair)
